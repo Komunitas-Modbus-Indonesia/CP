b4353ff11c965617400aed913b83a7dd7d4f7d19
VernSG <snexmania76@gmail.com>
2026-02-22 02:33:03 +0800

    Update: AboutSection.vue untuk Komunitas Modbus Indonesia
    Change-Id: I0ea331dafe19a3a0669fab0b1c4e76607813df17

--- src/vue/content/sections/AboutSection.vue
+++ src/vue/content/sections/AboutSection.vue
@@ -3,27 +3,27 @@
     <PageSection variant="default"
                  :id="props.id">
         <!-- Title -->
-        <PageSectionHeader title="*About* Template"
-                           subtitle="Get to know more about the project!"/>
+        <PageSectionHeader title="*Tentang* Kami"
+                           subtitle=""/>
 
         <!-- Content -->
         <PageSectionContent>
             <!-- About Template -->
-            <ArticleInfoBlock image="images/logo/agency-logo.png"
+            <ArticleInfoBlock image="images/logo/KMI.jpg"
                               :paragraphs="[
-                                  `<b>Ryan Balieiro</b> created <em>Foxy</em>, a free Vue3 and Bootstrap5 theme. The downloadable files consist of the entire source code, including HTML, CSS, and Javascript. Please feel free to download and customize the theme as you wish.`,
-                                  `The template available on <a href='github.com'>GitHub</a> is a valuable resource for developers, as it provides a free and accessible solution for various projects. Hosted on the platform, the template allows users to easily access and utilize the source code, making it a convenient starting point for web development endeavors.`,
-                                  `With this template, you have the freedom to create custom sections, fill pre-existing ones with your own content, and personalize the colors, fonts, and much more to your liking. In case you encounter any challenges while attempting to customize or run this template, please feel free to reach out to me without any hesitation. I would be more than happy to extend my assistance and help you out in every possible way.`
+                                  `<b>Komunitas Modbus Indonesia (KMI)</b> didedikasikan untuk standardisasi, edukasi, dan implementasi protokol komunikasi Modbus di lingkungan industri tanah air. Kami menjembatani kesenjangan antara akademisi, engineer lapangan, dan vendor teknologi.`,
+                                  `Di era Industri 4.0, protokol komunikasi yang handal menjadi fondasi penting dalam sistem otomasi industri. Modbus, sebagai salah satu protokol yang paling banyak digunakan, memerlukan pemahaman dan implementasi yang tepat untuk memaksimalkan efisiensi operasional.`,
+                                  `Melalui forum diskusi, workshop, dan kolaborasi dengan berbagai pemangku kepentingan, KMI berkomitmen untuk meningkatkan kompetensi profesional di bidang komunikasi industri dan mendorong adopsi standar terbaik dalam implementasi Modbus di Indonesia.`
                               ]"/>
         </PageSectionContent>
 
         <!-- Footer -->
-        <PageSectionFooter title="Get it for *free!*"
-                           description="Do you want to use and/or customize this template? Feel free to clone it from the official Github repo!"
+        <PageSectionFooter title="Bergabung Bersama *Kami*"
+                           description="Ingin menjadi bagian dari Komunitas Modbus Indonesia? Bergabunglah dengan kami dan tingkatkan kompetensi di bidang komunikasi industri!"
                            :include-divider="true"
-                           button-label="Download Now!"
-                           button-fa-icon="fa-solid fa-download"
-                           button-url="https://github.com/ryanbalieiro/vue-agency-landing-page-template"/>
+                           button-label="Join Us"
+                           button-fa-icon="fa-solid fa-user-plus"
+                           button-url="https://web.facebook.com/share/g/18GmXGAkYV/"/>
     </PageSection>
 </template>
 
