09fdd7cac610f7fa6c0db92cda0f489d4727e6a7
VernSG <snexmania76@gmail.com>
2026-02-22 02:33:03 +0800

    Update: HomePage.vue - ubah label navbar ke bahasa Indonesia
    Change-Id: Ib862f3582895807c1055cc13a6647b378d0ac3a4

--- src/vue/content/pages/HomePage.vue
+++ src/vue/content/pages/HomePage.vue
@@ -9,6 +9,7 @@ import PageWrapper from "/src/vue/components/layout/PageWrapper.vue"
 import SectionInfo from "/src/models/SectionInfo.js"
 import AboutSection from "/src/vue/content/sections/AboutSection.vue"
 import ContactSection from "/src/vue/content/sections/ContactSection.vue"
+import EventSection from "/src/vue/content/sections/EventSection.vue"
 import FaqSection from "/src/vue/content/sections/FaqSection.vue"
 import FeaturedProjectSection from "/src/vue/content/sections/FeaturedProjectSection.vue"
 import HeroSection from "/src/vue/content/sections/HeroSection.vue"
@@ -20,15 +21,16 @@ import TeamSection from "/src/vue/content/sections/TeamSection.vue"
 
 const sections = [
     new SectionInfo('hero',         HeroSection,            null,           null),
-    new SectionInfo('about',        AboutSection,           'About',        'pi pi-box'),
-    new SectionInfo('services',     ServicesSection,        'Services',     'pi pi-wrench'),
-    new SectionInfo('history',      HistorySection,         'History',      'pi pi-clock'),
-    new SectionInfo('featured',     FeaturedProjectSection, 'Zork',         'pi pi-qrcode'),
-    new SectionInfo('portfolio',    PortfolioSection,       'Portfolio',    'pi pi-folder-open'),
-    new SectionInfo('team',         TeamSection,            'Team',         'pi pi-users'),
-    new SectionInfo('faq',          FaqSection,             'Q&A',          'pi pi-question-circle'),
-    new SectionInfo('reviews',      ReviewsSection,         'Reviews',      'fa-regular fa-comment'),
-    new SectionInfo('contact',      ContactSection,         'Contact',      'fa-regular fa-envelope')
+    new SectionInfo('about',        AboutSection,           'Tentang',      'pi pi-box'),
+    // new SectionInfo('services',     ServicesSection,        'Services',     'pi pi-wrench'),
+    new SectionInfo('event',        EventSection,           'Event',        'fa-solid fa-calendar-days'),
+    // new SectionInfo('history',      HistorySection,         'History',      'pi pi-clock'),
+    // new SectionInfo('featured',     FeaturedProjectSection, 'Zork',         'pi pi-qrcode'),
+    new SectionInfo('portfolio',    PortfolioSection,       'Kegiatan',     'pi pi-folder-open'),
+    // new SectionInfo('team',         TeamSection,            'Team',         'pi pi-users'),
+    new SectionInfo('faq',          FaqSection,             'FAQ',          'pi pi-question-circle'),
+    // new SectionInfo('reviews',      ReviewsSection,         'Reviews',      'fa-regular fa-comment'),
+    // new SectionInfo('contact',      ContactSection,         'Contact',      'fa-regular fa-envelope')
 ]
 </script>
 
